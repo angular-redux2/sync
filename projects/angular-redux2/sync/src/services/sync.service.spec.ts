6afcfda754d922d6db04f1f6d87759c6f3dcad17
Garefild <Garefild2@gmail.com>
2023-04-23 02:28:34 +0300

    sync.service: add

---
+++ projects/angular-redux2/sync/src/services/sync.service.spec.ts
@@ -0,0 +1,396 @@
+/**
+ * Import third-party libraries
+ */
+
+import { NgRedux } from '@angular-redux2/store';
+
+/**
+ * angular-redux2/sync
+ */
+
+import { reduxSyncMiddleware, SyncService } from './sync.service';
+import type { ConfigSyncInterface } from '../interfaces/sync.interface';
+import { GET_INIT_STATE, RECEIVE_INIT_STATE } from '../interfaces/sync.interface';
+
+jest.mock('@angular-redux2/store');
+
+describe('SyncService', () => {
+    let sync: any;
+    let ngRedux: NgRedux<any>;
+    let mockBroadcastChannel: any;
+    let allowedSpy: jest.SpyInstance;
+
+    beforeEach(() => {
+        mockBroadcastChannel = {
+            onmessage: jest.fn(),
+            postMessage: jest.fn(),
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn()
+        };
+
+        window.BroadcastChannel = jest.fn(() => mockBroadcastChannel);
+
+        ngRedux = new NgRedux();
+        sync = new SyncService({
+            channelName: 'specTest',
+            predicate: jest.fn(),
+            whitelist: [ 'ALLOWED_ACTION' ],
+            blacklist: [ 'DISALLOWED_ACTION' ]
+        });
+
+        (NgRedux as any).store = ngRedux;
+        allowedSpy = jest.spyOn(sync, <any>'allowed');
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should initialize state from other tabs', () => {
+        const mockEvent = {
+            data: { type: RECEIVE_INIT_STATE, payload: { count: 0 } }
+        };
+
+        mockBroadcastChannel.addEventListener = ((event: string, func: any) => {
+            const messageEvent = new MessageEvent(event, mockEvent);
+            func(messageEvent);
+        });
+
+        sync.initializeState();
+
+        expect(ngRedux.dispatch).toHaveBeenCalledWith(
+            expect.objectContaining({
+                type: RECEIVE_INIT_STATE,
+                payload: expect.objectContaining({ count: 0 }),
+                $isSync: true,
+            }),
+        );
+    });
+
+    describe('constructor', () => {
+        test('should create a BroadcastChannel with the specified channelName', () => {
+            const settings: ConfigSyncInterface = {
+                channelName: 'test-channel',
+            };
+
+            const syncInstance: any = new SyncService(settings);
+            expect(globalThis.BroadcastChannel).toHaveBeenCalledWith('test-channel');
+            expect(syncInstance.channel).toBeDefined();
+        });
+
+        test('should call isActionAllowed and bind handleOnMessage to this', () => {
+            const syncInstance: any = new SyncService();
+
+            expect(syncInstance.allowed).toBeDefined();
+            expect(syncInstance.handleOnMessage).toBeDefined();
+            expect(syncInstance.handleOnMessage).toBeInstanceOf(Function);
+            expect(syncInstance.handleOnMessage({})).toBeUndefined();
+        });
+
+        test('should call initializeState if settings.initState is truthy', () => {
+            const settings: ConfigSyncInterface = {
+                initState: true,
+            };
+            const initializeStateSpy = jest.spyOn(SyncService.prototype, <any>'initializeState');
+            new SyncService(settings);
+
+            expect(initializeStateSpy).toHaveBeenCalledTimes(1);
+        });
+
+        test('should not call initializeState if settings.initState is falsy', () => {
+            const settings: ConfigSyncInterface = {
+                initState: false,
+            };
+            const initializeStateSpy = jest.spyOn(SyncService.prototype, <any>'initializeState');
+            new SyncService(settings);
+
+            expect(initializeStateSpy).not.toHaveBeenCalled();
+        });
+
+        test('should throw an error if the browser does not support cross tab communication', () => {
+            (window as any).BroadcastChannel = undefined;
+
+            expect(() => {
+                new SyncService();
+            }).toThrowError('Your browser doesn\'t support cross tab communication.');
+        });
+
+        test('should assign settings to this.settings', () => {
+            const settings: ConfigSyncInterface = {
+                channelName: 'test-channel',
+                initState: true,
+            };
+
+            const syncInstance: any = new SyncService(settings);
+            expect(syncInstance.settings).toEqual(expect.objectContaining({
+                channelName: 'test-channel',
+                prepareState: expect.any(Function),
+                initState: true,
+            }));
+        });
+
+        test('should not modify this.settings if no settings are passed in', () => {
+            const syncInstance: any = new SyncService();
+
+            expect(syncInstance.settings).toEqual(expect.objectContaining({
+                channelName: 'redux_state_sync',
+                prepareState: expect.any(Function),
+            }));
+        });
+    });
+
+    describe('handleOnMessage', () => {
+        let dispatchSpy: jest.SpyInstance;
+        let emitMessageSpy: jest.SpyInstance;
+
+        beforeEach(() => {
+            dispatchSpy = jest.spyOn(NgRedux.store, 'dispatch');
+            emitMessageSpy = jest.spyOn(sync, 'emitMessage');
+        });
+
+        test('should return early if no stampedAction is provided', () => {
+            const event = {
+                data: null,
+            } as MessageEvent;
+
+            sync.handleOnMessage(event);
+
+            expect(emitMessageSpy).not.toHaveBeenCalled();
+            expect(dispatchSpy).not.toHaveBeenCalled();
+        });
+
+        test('should return early if stampedAction.type is RECEIVE_INIT_STATE', () => {
+            const event = {
+                data: {
+                    type: 'RECEIVE_INIT_STATE',
+                },
+            } as MessageEvent;
+
+            sync.handleOnMessage(event);
+
+            expect(emitMessageSpy).not.toHaveBeenCalled();
+            expect(dispatchSpy).not.toHaveBeenCalled();
+        });
+
+        test('should emit RECEIVE_INIT_STATE message if stampedAction.type is GET_INIT_STATE', () => {
+            const event = {
+                data: {
+                    type: GET_INIT_STATE,
+                },
+            } as MessageEvent;
+
+            const prepareStateResult = { foo: 'bar' };
+            jest.spyOn((sync as any).settings, 'prepareState').mockReturnValue(prepareStateResult);
+
+            sync.handleOnMessage(event);
+
+            expect(emitMessageSpy).toHaveBeenCalledWith(expect.objectContaining({
+                type: RECEIVE_INIT_STATE,
+                payload: prepareStateResult,
+            }));
+            expect(dispatchSpy).not.toHaveBeenCalled();
+        });
+
+        test('should dispatch stampedAction if it is allowed', () => {
+            const event = {
+                data: {
+                    type: 'SOME_ACTION_TYPE',
+                    payload: { some: 'data' },
+                },
+            } as MessageEvent;
+
+            allowedSpy.mockReturnValueOnce(true);
+
+            sync.handleOnMessage(event);
+
+            expect(emitMessageSpy).not.toHaveBeenCalled();
+            expect(dispatchSpy).toHaveBeenCalledWith({
+                type: 'SOME_ACTION_TYPE',
+                payload: { some: 'data' },
+                $isSync: true,
+            });
+        });
+
+        test('should not dispatch stampedAction if it is not allowed', () => {
+            const event = {
+                data: {
+                    type: 'SOME_ACTION_TYPE',
+                    payload: { some: 'data' },
+                },
+            } as MessageEvent;
+
+            allowedSpy.mockReturnValueOnce(false);
+
+            sync.handleOnMessage(event);
+
+            expect(emitMessageSpy).not.toHaveBeenCalled();
+            expect(dispatchSpy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('emitMessage', () => {
+        test('should call postMessage with stampedAction when allowed', () => {
+            const action = { $wuid: '1234-1234', type: 'TEST_ACTION' };
+
+            allowedSpy.mockReturnValue(true);
+            jest.spyOn(sync, <any>'generateUidForAction').mockReturnValue(action);
+            sync.emitMessage({ type: 'TEST_ACTION' });
+
+            expect(allowedSpy).toHaveBeenCalledWith(action);
+            expect(mockBroadcastChannel.postMessage).toBeCalledWith(action);
+        });
+
+        test('should call postMessage with stampedAction when type is GET_INIT_STATE', () => {
+            const stampedAction = { type: GET_INIT_STATE };
+            allowedSpy.mockReturnValue(false);
+
+            sync.emitMessage(stampedAction);
+
+            expect(mockBroadcastChannel.postMessage).toHaveBeenCalledWith((sync as any).generateUidForAction(stampedAction));
+            expect(allowedSpy).toHaveBeenCalledWith((sync as any).generateUidForAction(stampedAction));
+        });
+
+
+        test('should call postMessage with stampedAction when type is RECEIVE_INIT_STATE', () => {
+            const stampedAction = { type: RECEIVE_INIT_STATE };
+            allowedSpy.mockReturnValue(false);
+
+            sync.emitMessage(stampedAction);
+
+            expect(mockBroadcastChannel.postMessage).toHaveBeenCalledWith((sync as any).generateUidForAction(stampedAction));
+            expect(allowedSpy).toHaveBeenCalledWith((sync as any).generateUidForAction(stampedAction));
+        });
+
+        test('should throw an error when browser does not support postMessage', () => {
+            const stampedAction = { type: RECEIVE_INIT_STATE };
+            mockBroadcastChannel.postMessage.mockImplementation(() => {
+                throw new Error();
+            });
+
+            expect(() => sync.emitMessage(stampedAction)).toThrow('Your browser doesn\'t support cross tab communication.');
+        });
+
+        test('should not call postMessage when not allowed', () => {
+            const stampedAction = { type: 'TEST_ACTION' };
+            allowedSpy.mockReturnValue(false);
+
+            sync.emitMessage(stampedAction);
+
+            expect(mockBroadcastChannel.postMessage).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('isActionAllowed', () => {
+        test('should return a function that always returns true if no settings are provided', () => {
+            const instance: any = new SyncService();
+            const allowed = instance.isActionAllowed();
+
+            const action = { type: 'SOME_ACTION' };
+            expect(allowed(action)).toBe(true);
+        });
+
+        test('should return a function that uses the provided predicate if it is a function', () => {
+            const predicate = jest.fn((action: any) => action.type !== 'BLACKLISTED_ACTION');
+            const settings = { predicate };
+            const instance: any = new SyncService(settings);
+            const allowed = instance.isActionAllowed();
+
+            const allowedAction = { type: 'ALLOWED_ACTION' };
+            expect(allowed(allowedAction)).toBe(true);
+
+            const blacklistedAction = { type: 'BLACKLISTED_ACTION' };
+            expect(allowed(blacklistedAction)).toBe(false);
+
+            expect(predicate).toHaveBeenCalledTimes(2);
+            expect(predicate).toHaveBeenCalledWith(allowedAction);
+            expect(predicate).toHaveBeenCalledWith(blacklistedAction);
+        });
+
+        test('should return a function that allows all actions not in the blacklist array', () => {
+            const blacklist = [ 'BLACKLISTED_ACTION_1', 'BLACKLISTED_ACTION_2' ];
+            const settings = { blacklist };
+            const instance: any = new SyncService(settings);
+            const allowed = instance.isActionAllowed();
+
+            const allowedAction = { type: 'ALLOWED_ACTION' };
+            expect(allowed(allowedAction)).toBe(true);
+
+            const blacklistedAction1 = { type: 'BLACKLISTED_ACTION_1' };
+            expect(allowed(blacklistedAction1)).toBe(false);
+
+            const blacklistedAction2 = { type: 'BLACKLISTED_ACTION_2' };
+            expect(allowed(blacklistedAction2)).toBe(false);
+        });
+
+        test('should return a function that only allows actions in the whitelist array', () => {
+            const whitelist = [ 'ALLOWED_ACTION_1', 'ALLOWED_ACTION_2' ];
+            const settings = { whitelist };
+            const instance: any = new SyncService(settings);
+            const allowed = instance.isActionAllowed();
+
+            const allowedAction1 = { type: 'ALLOWED_ACTION_1' };
+            expect(allowed(allowedAction1)).toBe(true);
+
+            const allowedAction2 = { type: 'ALLOWED_ACTION_2' };
+            expect(allowed(allowedAction2)).toBe(true);
+
+            const notAllowedAction = { type: 'NOT_ALLOWED_ACTION' };
+            expect(allowed(notAllowedAction)).toBe(false);
+        });
+    });
+
+    describe('reduxSyncMiddleware', () => {
+        let middleware: ReturnType<typeof reduxSyncMiddleware>;
+        let spyEmitMessage: jest.SpyInstance;
+
+        beforeEach(() => {
+            spyEmitMessage = jest.spyOn(SyncService.prototype, 'emitMessage');
+            middleware = reduxSyncMiddleware();
+        });
+
+        it('should call emitMessage when action is dispatched', () => {
+            const action = { type: 'SOME_ACTION' };
+            const next = jest.fn();
+            spyEmitMessage.mockReturnValue(true);
+
+            middleware(null, action, next);
+
+            expect(spyEmitMessage).toHaveBeenCalledWith(action);
+            expect(next).toHaveBeenCalledWith(null, {
+                ...action,
+                $isSync: false,
+            });
+        });
+
+        it('should call emitMessage with GET_INIT_STATE when initState is true', () => {
+            middleware = reduxSyncMiddleware({ initState: true });
+
+            expect(spyEmitMessage).toHaveBeenCalledWith({
+                type: GET_INIT_STATE,
+            });
+        });
+
+        it('should call next with RECEIVE_INIT_STATE action', () => {
+            const state = {};
+            const action = { type: 'xxx', payload: { some: 'state' } };
+            const next = jest.fn();
+
+            middleware(state, action, next);
+
+            expect(next).toHaveBeenCalledWith(state, { type: 'xxx', $isSync: false, payload: { some: 'state' } });
+        });
+
+        it('should call next with modified action', () => {
+            const state = {};
+            const action = { type: 'SOME_ACTION' };
+            const next = jest.fn();
+
+            middleware(state, action, next);
+
+            expect(next).toHaveBeenCalledWith(state, {
+                ...action,
+                $isSync: false,
+            });
+        });
+    });
+});
---
+++ projects/angular-redux2/sync/src/services/sync.service.ts
@@ -0,0 +1,238 @@
+/**
+ * Import third-party libraries
+ */
+
+import type { AnyAction } from 'redux';
+import { Middleware, NgRedux } from '@angular-redux2/store';
+
+/**
+ * angular-redux2/sync
+ */
+
+import { guid } from '../components/uid.component';
+import type { ConfigSyncInterface } from '../interfaces/sync.interface';
+import { GET_INIT_STATE, RECEIVE_INIT_STATE } from '../interfaces/sync.interface';
+import { NextMiddleware } from "@angular-redux2/store/interfaces/reducer.interface";
+
+/**
+ * The SyncComponent class allows synchronization of Redux state between different
+ * tabs/windows in the same browser.
+ */
+export class SyncService {
+    /**
+     * The unique identifier of the current tab.
+     * @private
+     */
+
+    private readonly windowID = guid();
+
+    /**
+     * The channel used for communication between tabs.
+     * @private
+     */
+
+    private readonly channel: BroadcastChannel;
+
+    /**
+     * A function to check if a specific action is allowed.
+     * @private
+     */
+
+    private readonly allowed: (action: AnyAction) => boolean;
+
+    /**
+     * The default settings for the SyncComponent.
+     * @private
+     */
+
+    private readonly settings: any = {
+        channelName: 'redux_state_sync',
+        broadcastChannelOption: {},
+        prepareState: (state: any) => state
+    };
+
+    /**
+     * Creates a new instance of SyncService.
+     *
+     * @param {ConfigSyncInterface} [settings] - The optional settings for the SyncComponent.
+     * @throws {Error} Throws an error if the browser doesn't support cross-tab communication.
+     */
+
+    constructor(settings?: ConfigSyncInterface) {
+        if (settings) {
+            this.settings = Object.assign(this.settings, settings);
+        }
+
+        try {
+            this.channel = new BroadcastChannel(this.settings.channelName);
+            this.allowed = this.isActionAllowed();
+            this.channel.onmessage = this.handleOnMessage.bind(this);
+
+            if (settings?.initState) {
+                this.initializeState();
+            }
+        } catch (e) {
+            throw new Error('Your browser doesn\'t support cross tab communication.');
+        }
+    }
+
+    /**
+     * Handle incoming messages from the broadcast channel and dispatch synced actions.
+     * @param {MessageEvent} event - The message event object containing the action payload.
+     * @returns {void}
+     */
+
+    handleOnMessage(event: MessageEvent): void {
+        const stampedAction = event.data;
+
+        if (!stampedAction) {
+            return;
+        }
+
+        switch (stampedAction.type) {
+            case RECEIVE_INIT_STATE:
+                return;
+
+            case GET_INIT_STATE:
+                this.emitMessage({
+                    type: RECEIVE_INIT_STATE,
+                    payload: this.settings.prepareState(NgRedux.store.getState())
+                });
+
+                return;
+        }
+
+        if (this.allowed(stampedAction)) {
+            NgRedux.store.dispatch(
+                Object.assign(stampedAction, {
+                    $isSync: true,
+                }),
+            );
+        }
+    }
+
+    /**
+     * Sends a stamped action through the broadcast channel.
+     * @param {any} stampedAction - The stamped action to be sent.
+     * @returns {void}
+     * @throws {Error} If the browser doesn't support cross-tab communication.
+     */
+
+    emitMessage(stampedAction: any): void {
+        stampedAction = this.generateUidForAction(stampedAction);
+
+        try {
+            if (
+                this.allowed(stampedAction) ||
+                stampedAction.type === GET_INIT_STATE ||
+                stampedAction.type === RECEIVE_INIT_STATE
+            ) {
+                this.channel.postMessage(stampedAction);
+            }
+        } catch (e) {
+            throw new Error('Your browser doesn\'t support cross tab communication.');
+        }
+    }
+
+    /**
+     * Initializes the state by dispatching an action to retrieve the initial state from other tabs.
+     * @private
+     * @returns {void}
+     */
+
+    private initializeState() {
+        const initializeEvent = (event: MessageEvent): void => {
+            if (event.data.type === RECEIVE_INIT_STATE) {
+                NgRedux.store.dispatch(
+                    Object.assign(event.data, {
+                        $isSync: true,
+                    }),
+                );
+
+                this.channel.removeEventListener('message', initializeEvent, false);
+            }
+        };
+
+        this.channel.addEventListener('message', initializeEvent);
+    }
+
+    /**
+     * Generates a unique identifier for the given action by adding a $wuid property with the current window ID.
+     * @param {any} action - The action to be stamped with a unique identifier.
+     * @returns {AnyAction} - The stamped action object.
+     */
+
+    private generateUidForAction(action: any): AnyAction {
+        const stampedAction = action;
+        stampedAction.$wuid = this.windowID;
+
+        return stampedAction;
+    }
+
+    /**
+     * Checks whether the action is allowed based on the settings.
+     * @returns A function that takes an action and returns a boolean value indicating whether the action is allowed.
+     */
+
+    private isActionAllowed(): (action: AnyAction) => boolean {
+        const settings = this.settings;
+        let allowed = (action: AnyAction) => true;
+
+        if (settings.predicate && typeof settings.predicate === 'function') {
+            allowed = settings.predicate;
+        } else if (Array.isArray(settings.blacklist)) {
+            allowed = (action) => settings.blacklist.indexOf(action.type) < 0;
+        } else if (Array.isArray(settings.whitelist)) {
+            allowed = (action) => settings.whitelist.indexOf(action.type) >= 0;
+        }
+
+        return allowed;
+    }
+}
+
+/**
+ * Sync redux state across browser tabs
+ * A lightweight middleware to sync your redux state across browser tabs.
+ * It will listen to the Broadcast Channel and dispatch the same actions dispatched in other tabs to keep the redux state in sync.
+ *
+ * @example
+ * ```typescript
+ * constructor(ngRedux: NgRedux<IAppState>, devTools: DevToolsExtension) {
+ *     let enhancer: Array<any> = [];
+ *
+ *     if (devTools.enhancer() && isDevMode())
+ *         enhancer = [ devTools.enhancer() ];
+ *
+ *     ngRedux.configureStore(<any> rootReducer, INITIAL_STATE, [
+ *         reduxSyncMiddleware({ initState: true })
+ *     ], enhancer);
+ * }
+ * ```
+ *
+ * `Before you use`
+ * >Please take note that BroadcastChannel can only send data that is supported by the structured clone algorithm (`Strings`, `Objects`, `Arrays`, `Blobs`, `ArrayBuffer`, `Map`),
+ * so you need to make sure that the actions that you want to send to other tabs don't include any functions in the payload.`
+ */
+
+export function reduxSyncMiddleware(settings?: ConfigSyncInterface): Middleware {
+    const sync = new SyncService(settings);
+
+    if (settings?.initState) {
+        sync.emitMessage({ type: GET_INIT_STATE });
+    }
+
+    return (state: any, action: any, next: NextMiddleware<any>): void => {
+        if (action && !action.$wuid)
+            sync.emitMessage(action);
+
+        if (action.type === RECEIVE_INIT_STATE) {
+            return action['payload'];
+        }
+
+        return next(state,
+            Object.assign(action, {
+                $isSync: typeof action.$isSync === 'undefined' ? false : action.$isSync
+            })
+        );
+    };
+}
